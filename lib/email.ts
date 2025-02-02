ef1a646cdad06c390530a1735f3a9654087e375f
Aarush Tahilani <aarushtahiliani8@gmail.com>
2025-02-01 20:44:23 -0800

    final

--- lib/email.ts
+++ lib/email.ts
@@ -4,11 +4,19 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendFormData(formData: any) {
+type FormDataType = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    subject: string;
+    message: string;
+  };
+  
+  export async function sendFormData(formData: FormDataType) {  
     try {
       console.log("Received form data:", formData);
 
-      const { data, error } = await resend.emails.send({
+      const { error } = await resend.emails.send({
         from: `${formData.firstName} ${formData.lastName} <${process.env.RESEND_FROM_EMAIL}>`,
         to: "6speedphoto.multimedia@gmail.com",
         subject: formData.subject,
